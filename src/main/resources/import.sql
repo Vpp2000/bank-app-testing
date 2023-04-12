c7741e905d4732e73ac591ea139f59b026718fe3
Vpp2000 <victor.ponce.p@uni.pe>
2023-04-12 12:13:39 -0500

    swagger is working properly

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO accounts (owner_name, balance) VALUES ('Victor', 1000);
+INSERT INTO accounts (owner_name, balance) VALUES ('Andres', 2000);
+INSERT INTO accounts (owner_name, balance) VALUES ('Jose', 3000);
+INSERT INTO banks (name, total_transfers) VALUES ('Banco Financiero', 0);

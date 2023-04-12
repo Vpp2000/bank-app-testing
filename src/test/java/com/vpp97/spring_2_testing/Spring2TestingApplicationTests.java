102eeeccb1f6533e2979f5cb159da532d7743513
Vpp2000 <victor.ponce.p@uni.pe>
2023-04-11 21:28:49 -0500

    added test of money transfer

--- src/test/java/com/vpp97/spring_2_testing/Spring2TestingApplicationTests.java
+++ src/test/java/com/vpp97/spring_2_testing/Spring2TestingApplicationTests.java
@@ -6,6 +6,7 @@ import com.vpp97.spring_2_testing.repositories.BankRepository;
 import com.vpp97.spring_2_testing.services.AccountService;
 import com.vpp97.spring_2_testing.services.AccountServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,7 +32,8 @@ class Spring2TestingApplicationTests {
     }
 
     @Test
-    void contextLoads() {
+    @DisplayName("Test money transfer")
+    void test_money_transfer() {
         when(accountRepository.findById(1L)).thenReturn(Data.ACCOUNT_001);
         when(accountRepository.findById(2L)).thenReturn(Data.ACCOUNT_002);
         when(bankRepository.findById(1L)).thenReturn(Data.BANK);
@@ -40,5 +42,15 @@ class Spring2TestingApplicationTests {
         BigDecimal targetBalance = accountService.checkBalance(2L);
 
         assertEquals("1000", sourceBalance.toPlainString());
+        assertEquals("2000", targetBalance.toPlainString());
+
+        accountService.transfer(1L, 1L, 2L, new BigDecimal("300"));
+
+        sourceBalance = accountService.checkBalance(1L);
+        targetBalance = accountService.checkBalance(2L);
+
+        assertEquals("700", sourceBalance.toPlainString());
+        assertEquals("2300", targetBalance.toPlainString());
+
     }
 }
